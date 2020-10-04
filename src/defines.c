bd61c005125c2fdda2916f1f7bacc058c9f1a2cb
baskiton <baskiton@mail.ru>
2020-10-05 02:23:45 +0700

    v1.1.0 Add new features Optimized import

--- src/defines.c
+++ src/defines.c
@@ -1,8 +1,44 @@
-#include <stdint.h>
 #include <avr/io.h>
-#include <math.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 #include "defines.h"
+
+struct freelist {
+    size_t sz;
+    struct freelist *nx;
+};
+
+extern struct freelist *__flp;
+extern uint8_t *__brkval;
+
+/*!
+ * @brief Get approximate free memory space
+ * @return Amount of free memory
+ */
+int32_t get_free_mem_size(void) {
+    int32_t free_sz;
+    struct freelist *next;
+    uint16_t brkval;
+
+    brkval = (uint16_t)__brkval;
+    if (!brkval)
+        brkval = (uint16_t)__malloc_heap_start;
+
+    free_sz = (int32_t)SP - (int32_t)brkval - (int32_t)__malloc_margin;
+
+    next = __flp;
+    while (next) {
+        free_sz += next->sz + 2;
+        next = next->nx;
+    }
+    
+    return free_sz;
+}
 
 /*!
  * @brief Setting pin to \c INPUT or \c OUTPUT
@@ -24,4 +60,22 @@ void set_pin_mode(volatile uint8_t *port, uint8_t bit, uint8_t mode) {
         set_output(*(port - 1), bit);
     
     SREG = sreg;
+}
+
+/*!
+ * @brief Reversing bits on an octet.
+ * Method attributed to Rich Schroeppel in the Programming Hacks section
+ * http://www.inwap.com/pdp10/hbaker/hakmem/hakmem.html
+ * http://www.inwap.com/pdp10/hbaker/hakmem/hacks.html#item167
+ * 
+ * @example:
+ *   1 byte
+ * ____________
+ *  0110  0001
+ *      ||
+ *      \/
+ *  1000  0110
+ */
+uint8_t bits_swap(uint8_t data) {
+    return (uint8_t)(((uint64_t)data * 0x0202020202U & 0x010884422010U) % 0x3ffU);
 }
--- src/defines.h
+++ src/defines.h
@@ -2,14 +2,13 @@
 #define DEFINES_H
 
 #include <avr/io.h>
-#include <avr/builtins.h>
+
+#include <stdint.h>
 
 #define INPUT 0x00
 #define OUTPUT 0x01
 #define INPUT_PULLUP 0x02
 
-#define cli() __builtin_avr_cli()
-#define sti() __builtin_avr_sei()
 #define nop() __asm__ __volatile__("nop")
 
 #define bit_read(value, bit) (((value) >> (bit)) & 1U)
@@ -20,6 +19,13 @@
 #define set_output(port, bit) ((port) |= (1U << (bit)))
 #define set_input(port, bit) ((port) &= ~(1U << (bit)))
 
+struct avr_pin_s {
+    uint8_t pin_num;
+    volatile uint8_t *port;
+};
+
 extern void set_pin_mode(volatile uint8_t *port, uint8_t bit, uint8_t mode);
+extern int32_t get_free_mem_size(void);
+extern uint8_t bits_swap(uint8_t data);
 
 #endif  /* !DEFINES_H */
